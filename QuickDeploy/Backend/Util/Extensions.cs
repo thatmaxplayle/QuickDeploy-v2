968d7edf284b3701ea2581d7479d3e0b8c22828d
Max Playle <maxplayle04@gmail.com>
2022-08-23 02:16:09 +0100

    Add necessary extension methods.

---
+++ QuickDeploy/Backend/Util/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QuickDeploy.Backend.Enums;
+
+namespace QuickDeploy.Backend.Util
+{
+    public static class Extensions
+    {
+
+        public static bool ToBooleanValue(this ValidationResult vr)
+        {
+            return vr == ValidationResult.Valid;
+        }
+
+        public static string GetSafeFileName(this string input)
+        {
+            char[] illegalChars = new char[] { '#', '%', '<', '>', '$', '+', '!', '`', '&', '*', '\'', '|', '{', '}', '?', '\"', '=', '@' };
+            foreach (var c in illegalChars)
+                input = input.Replace(c.ToString(), "");
+
+            return input;
+        }
+
+    }
+}
