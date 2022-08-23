481e19a863d11d8e371349cde11aeac593cd360a
Max Playle <maxplayle04@gmail.com>
2022-08-23 02:18:07 +0100

    Minor changes to MainWindow

--- QuickDeploy/MainWindow.xaml.cs
+++ QuickDeploy/MainWindow.xaml.cs
@@ -1,7 +1,14 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 using QuickDeploy.Backend;
 using QuickDeploy.Controls;
+using QuickDeploy.Dialogs;
 
 namespace QuickDeploy
 {
@@ -13,7 +20,9 @@ namespace QuickDeploy
         public MainWindow()
         {
             InitializeComponent();
-            Test();
+            //Test();
+
+            LoadDeployments();
         }
 
         void Test()
@@ -40,6 +49,68 @@ namespace QuickDeploy
 
             spDeployments.Children.Add(visualDeploymentEnabled);
 
+        }
+
+        private void LoadDeployments()
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+
+            worker.DoWork += (sender, args) =>
+            {
+                Console.WriteLine("[MainWindow] Loading deployments...");
+
+                // Get all QDF files.
+                var files = new DirectoryInfo("deployments").GetFiles("*.qdf");
+                Console.WriteLine("Found {0} potential deployments.", files.Length);
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine("Attempting to load deployment file {0}...", file.Name);
+
+                    var canLoad = Deployment.TryLoad(file.Name, out Deployment? depl);
+                    
+                    if (canLoad && depl != null)
+                    {
+                        depl.CanDeployNow = depl.EvaluateEnsureFiles();
+
+                        Console.WriteLine("[INFO] Successfully loaded deployment {0} from file {1}!", depl.Name, file.Name);
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            spDeployments.Children.Add(new VisualDeployment(depl));
+                        });
+                    }
+                    else
+                    {
+                        Console.WriteLine("[ERROR] Failed to load deployment {0}!", file.Name);
+                    }
+
+                }
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    if (spDeployments.Children.Count <= 0)
+                    {
+                        spDeployments.Children.Add(new TextBlock
+                        {
+                            VerticalAlignment = VerticalAlignment.Center,
+                            Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255)),
+                            TextWrapping = TextWrapping.WrapWithOverflow,
+                            Text = "There are no deployments. Why not create a new one using the New Deployment button above?!",
+                        });
+                    }
+                });
+            };
+
+            Task.Run(worker.RunWorkerAsync);
+        }
+
+        private void btnNewDeployment_Click(object sender, RoutedEventArgs e)
+        {
+            NewDeployment newDeployment = new NewDeployment()
+            {
+                Owner = this
+            };
+            newDeployment.ShowDialog();
         }
     }
 }
