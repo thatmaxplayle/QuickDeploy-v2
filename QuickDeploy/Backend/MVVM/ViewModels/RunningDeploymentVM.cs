793588b3ca0bfc19d2250f423a44f609b41992b4
Max Playle <maxplayle04@gmail.com>
2022-08-23 02:15:30 +0100

    Added a new view model for the running deployment window

---
+++ QuickDeploy/Backend/MVVM/ViewModels/RunningDeploymentVM.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickDeploy.Backend.MVVM.ViewModels
+{
+    internal class RunningDeploymentVM : VMBase
+    {
+
+        public RunningDeploymentVM()
+        {
+            _closeBtnAvailable = false;
+            CurrentStatus = "Starting...";
+            _windowTitle = "Running Deployment...";
+        }
+
+        private string? _currentStatus;
+        public string? CurrentStatus
+        {
+            get => _currentStatus;
+            set
+            {
+                if (value != _currentStatus)
+                {
+                    _currentStatus = value;
+                    OnPropertyChanged(nameof(CurrentStatus));
+                }
+            }
+        }
+
+        private bool _closeBtnAvailable;
+        public bool CloseButtonAvailable
+        {
+            get => _closeBtnAvailable;
+            set
+            {
+                if (_closeBtnAvailable != value)
+                {
+                    _closeBtnAvailable = value;
+                    OnPropertyChanged(nameof(CloseButtonAvailable));
+                }
+            }
+        }
+
+        private string _windowTitle;
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set
+            {
+                if (value != _windowTitle)
+                {
+                    _windowTitle = value;
+                    OnPropertyChanged(nameof(WindowTitle));
+                }
+            }
+        }
+
+    }
+}
