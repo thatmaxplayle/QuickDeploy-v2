b52ca96a72b4ecf355f812c15eeaacc4d88977b3
Max Playle <maxplayle04@gmail.com>
2022-08-23 02:15:19 +0100

    Add a new view model for the CreateDeployment window

---
+++ QuickDeploy/Backend/MVVM/ViewModels/CreateDeploymentVM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace QuickDeploy.Backend.MVVM.ViewModels
+{
+    internal class CreateDeploymentVM : VMBase
+    {
+
+        public CreateDeploymentVM()
+        {
+            _destinationDirectorySelectForeground = new SolidColorBrush(Color.FromArgb(255,255,0,0));
+            _sourceDriectorySelectForeground = new SolidColorBrush(Color.FromArgb(255,255,0,0));
+        }
+
+        private SolidColorBrush _destinationDirectorySelectForeground;
+        public SolidColorBrush DestinationDirectorySelectForeground
+        {
+            get => _destinationDirectorySelectForeground;
+            set
+            {
+                if (value != _destinationDirectorySelectForeground)
+                {
+                    _destinationDirectorySelectForeground = value;
+                    OnPropertyChanged(nameof(DestinationDirectorySelectForeground));
+                }
+            }
+        }
+
+        private SolidColorBrush _sourceDriectorySelectForeground;
+        public SolidColorBrush SourceDirectorySelectForeground
+        {
+            get => _sourceDriectorySelectForeground;
+            set
+            {
+                if (value != _sourceDriectorySelectForeground)
+                {
+                    _sourceDriectorySelectForeground = value;
+                    OnPropertyChanged(nameof(SourceDirectorySelectForeground));
+                }
+            }
+        }
+
+        
+
+    }
+}
