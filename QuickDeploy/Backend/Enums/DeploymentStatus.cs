28627734216a73bfa565dc676a46b7cd1dd22e47
Max Playle <maxplayle04@gmail.com>
2022-08-23 02:14:50 +0100

    Add a deployment status enum so progress can be updated on the UI via an event handler.

---
+++ QuickDeploy/Backend/Enums/DeploymentStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickDeploy.Backend.Enums
+{
+    internal enum DeploymentStatus
+    {
+
+        ValidatingDeployment,
+        ValidatingEnsureFiles,
+        CopyingFiles,
+        CopyingFolders,
+
+        COMPLETE,
+        FAILED,
+
+    }
+}
